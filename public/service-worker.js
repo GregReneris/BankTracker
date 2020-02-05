2e31bd2505b8ddac7e399ec61421e5dd0d9146e1
Gregory Reneris <greg@reneris.com>
2020-02-05 15:40:45 -0800

    adding offline functionality.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "db.js", "style.css", "./routes/api.js", "favicon.ico"];
+const FILES_TO_CACHE = ["/", "/index.html", "/db.js", "/style.css", "/favicon.ico", "/index.js"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
@@ -34,7 +34,41 @@ self.addEventListener("activate", function(evt) {
 });
 
 // fetch
+// self.addEventListener("fetch", function(evt) {
+//   evt.respondWith(
+//     caches.open(CACHE_NAME).then(cache => {
+//       return cache.match(evt.request).then(response => {
+//         return response || fetch(evt.request);
+//       });
+//     })
+//   );
+// });
 self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            
+          });
+      }).catch(err => {
+        console.log(err)
+      })
+    );
+
+    return;
+  }
+
   evt.respondWith(
     caches.open(CACHE_NAME).then(cache => {
       return cache.match(evt.request).then(response => {
